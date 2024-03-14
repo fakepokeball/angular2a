f412626b1f67b52e6b202876fe9b851cc9ef8dc1
Güven Çabat <gvncbt@gmail.com>
2024-03-14 17:44:16 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormComponent } from './components/form/form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'angular2a';
+  title = 'Tobeto';
+  count = 0;
+  imputValue: string = '';
+  obBtnClick(event: Event) {
+    console.log(this.imputValue);
+    this.count++;
+  }
+  onChange(event: Event) {
+    let element = event.target as HTMLInputElement;
+    console.log('Input value is changed', element.value);
+    this.imputValue = element.value;
+  }
 }
